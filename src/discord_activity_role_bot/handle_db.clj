063946034a8cd6cb0273ae7322a8c820be03e73a
Barak <18008770+Lezyes@users.noreply.github.com>
2023-07-02 02:27:32 +0300

    more funcs

--- src/discord_activity_role_bot/handle_db.clj
+++ src/discord_activity_role_bot/handle_db.clj
@@ -17,9 +17,6 @@
     (println db-comment)
     (pprint db))))
 
-
-(save-db ndb)
-
 (defn load-db [] (edn/read-string (slurp db-file-path)))
 
 (defn lower_names [db] 
@@ -33,7 +30,36 @@
  (let [updated-db (update-in db [server-id rule-id] (fn [old-rule] new-rule))]
       updated-db)) 
 
-(ppr db)
+(defn add-names-to-rule [db server-id rule-id names]
+ (let [updated-db (transform [server-id rule-id :names] #(concat % (map string/lower-case names)) db)]
+      updated-db)) 
+
+
+(defn remove-names-from-rule [db server-id rule-id names]
+ (let [has-names? (fn [rule-names] 
+                   (filter 
+                    (fn [rule-name] 
+                     (some #(not= % rule-name) 
+                      names)) 
+                    rule-names))
+       updated-db (transform [server-id rule-id :names] has-names?  db)]
+      updated-db)) 
+
+(defn update-rule-names [db server-id rule-id new-names]
+ (let [updated-db (update-in db [server-id rule-id :names] (fn [old-names] (map string/lower-case new-names)))]
+      updated-db)) 
+
+
+
+(update-rule-names db
+          "609603874474426372" "1087888852280164372" ["qUAke", "New", "bazooka"])
+
+
+
+(remove-names-from-rule db
+          "609603874474426372" "1087888852280164372" ["quake"])
+
+(some #(= % "quake") ["quake" "diabotical"])
 
 
 
