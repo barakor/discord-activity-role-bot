48e8c0c48176f156a5c73cf0fce10c69f8f65e1a
Barak <18008770+Lezyes@users.noreply.github.com>
2023-12-04 06:00:39 +0200

    optimization

--- src/discord_activity_role_bot/handle_presence.clj
+++ src/discord_activity_role_bot/handle_presence.clj
@@ -25,36 +25,34 @@
  (let [user-id (get-in event-data [:user :id])
        event-guild-id (:guild-id event-data)
        guild-roles-rules (get-in @db [event-guild-id :roles-rules])
-       activities-names (->> event-data 
-                          (s/select [:activities s/ALL :name #(not= % "Custom Status")])
-                          (map string/lower-case)
-                          (set))
+       supervised-roles-ids (set (keys guild-roles-rules))]
 
-       supervised-roles-ids (set (keys guild-roles-rules))
-       
-       anything-roles-rules (->> guild-roles-rules
-                              (s/select [s/ALL #(= :else (:type (second %)))]) 
-                              (map first))
+   (when (not-empty supervised-roles-ids)
+     (let[activities-names (->> event-data 
+                             (s/select [:activities s/ALL :name #(not= % "Custom Status")])
+                             (map string/lower-case)
+                             (set))
+          anything-roles-rules (->> guild-roles-rules
+                                 (s/select [s/ALL #(= :else (:type (second %)))]) 
+                                 (map first))
+          relavent-roles-rules (->> guild-roles-rules
+                                 (s/select [s/ALL #(not-empty (contains-subset activities-names (:activity-names (second %))))])   
+                                 (map first))
+          new-roles-ids (cond 
+                          (empty? activities-names) #{}
+                          (not-empty relavent-roles-rules) (set relavent-roles-rules)
+                          :else (set anything-roles-rules))]
 
-       relavent-roles-rules (->> guild-roles-rules
-                              (s/select [s/ALL #(not-empty (contains-subset activities-names (:activity-names (second %))))])   
-                              (map first))
-
-       new-roles-ids (cond 
-                       (empty? activities-names) #{}
-                       (not-empty relavent-roles-rules) (set relavent-roles-rules)
-                       :else (set anything-roles-rules))]
-
-     (println "event-guild-id: " event-guild-id 
-              "\n"
-              "user-id: " user-id
-              "\n"
-              "activities-names: " activities-names
-              "\n"
-              "supervised-roles-ids(removing all): " supervised-roles-ids
-              "\n"
-              "roles-to-add: " new-roles-ids 
-              "\n")
-     
-     (update-user-roles rest-connection event-guild-id user-id new-roles-ids supervised-roles-ids)))
+        (println "event-guild-id: " event-guild-id 
+                 "\n"
+                 "user-id: " user-id
+                 "\n"
+                 "activities-names: " activities-names
+                 "\n"
+                 "supervised-roles-ids(removing all): " supervised-roles-ids
+                 "\n"
+                 "roles-to-add: " new-roles-ids 
+                 "\n")
+        
+        (update-user-roles rest-connection event-guild-id user-id new-roles-ids supervised-roles-ids)))))
 
